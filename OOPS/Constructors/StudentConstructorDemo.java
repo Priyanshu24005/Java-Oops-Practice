4560e0655851904421a6ee9bf6fef2f0f31195dc
Priyanshu Kumar Jha <anshu24005@gmail.com>
2026-01-17 12:59:01 +0530

    Added Student constructor demo

--- OOPS/Constructors/StudentConstructorDemo.java
+++ OOPS/Constructors/StudentConstructorDemo.java
@@ -1,6 +1,3 @@
-// // Write a Java program to demonstrate the use of a default constructor.
-// Create a StudentInfo class with instance variables such as roll number, name, and course.
-// Initialize these variables using a default constructor and display the student details by creating an object of the class.
 
 class StudentInfo {
 
@@ -8,23 +5,54 @@ class StudentInfo {
     String name;
     String course;
 
+    //default Constructor
     StudentInfo() {
         roll = 22;
         name = "Anshu";
         course = "BCA";
     }
 
+    //parameterized Constructor
+    StudentInfo(int roll, String name, String course) {
+        this.roll = roll;
+        this.name = name;
+        this.course = course;
+    }
+
+    //copy Consturctor
+    StudentInfo(StudentInfo s3) {
+        this.roll = s3.roll;
+        this.name = s3.name;
+        this.course = s3.course;
+    }
+
     void display() {
         System.out.println("Roll no = " + roll);
         System.out.println("Name = " + name);
-        System.out.println("Course = " + course);
+        System.out.println("Course = " + course + "\n");
     }
+
 }
 
 class StudentConstructorDemo {
 
     public static void main(String args[]) {
         StudentInfo s1 = new StudentInfo();
+        StudentInfo s2 = new StudentInfo(22, "Anshu", "BCA");
+        StudentInfo s3 = new StudentInfo(s2);
+
+        System.out.println("Before Modifying\n");
         s1.display();
+        s2.display();
+        s3.display();
+
+        s2.roll = 30;
+        s2.name = "Priyanshu";
+        s2.course = "MBA";
+
+        System.out.println("After modifying\n");
+        s1.display();
+        s2.display();
+        s3.display();
     }
 }
